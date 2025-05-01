2d3fff894666dafff3fa05c76320aaede45b1841
kostyaslob <kostyaslob@gmail.com>
2025-05-01 17:34:11 +0300

    add imports, update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,21 @@
 import './App.css';
-import { useEffect, useState } from 'react';
 
 import ContactForm from "./ContactForm/ContactForm";
 import SearchBox from "./SearchBox/SearchBox";
 import ContactList from "./ContactList/ContactList";
-import Contact from "./Contact/Contact";
+import initialContacts from "../contacts.json";
+
+// import { useEffect, useState } from 'react';
+
 
 export default function App() {
   
   return (
-    <>
+    <div>
+      <h1>Phonebook</h1>
       <ContactForm />
       <SearchBox />
       <ContactList />
-      <Contact />
-      
-    </>
+    </div>
   );
 };
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+// import css from "./Contact.module.css";
+
+export default function Contact() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
