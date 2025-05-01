84bd8444e48a1eea5d7073fe63d0d2050d296000
kostyaslob <kostyaslob@gmail.com>
2025-05-01 17:12:06 +0300

    add components and libraries

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,20 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
-import Description from "./Description/Description";
-import Options from "./Options/Options";
-import Feedback from "./Feedback/Feedback";
-import Notification from "./Notification/Notification";
+import ContactForm from "./ContactForm/ContactForm";
+import SearchBox from "./SearchBox/SearchBox";
+import ContactList from "./ContactList/ContactList";
+import Contact from "./Contact/Contact";
 
 export default function App() {
-  const [values, setValues] = useState(() => {
-    const savedValues = window.localStorage.getItem("key");
-      if (savedValues !== null) {
-      return JSON.parse(savedValues);
-  }
-  return {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
-})
-
-  useEffect(() => {
-    window.localStorage.setItem("key", JSON.stringify(values));
-  }, [values]);
-
-  let totalFeedback = values.good + values.neutral + values.bad;
-  let positiveFeedback = Math.round((values.good / totalFeedback) * 100);
-
-  const updateFeedback = feedbackType => {
-    setValues(values => ({
-      ...values,
-      [feedbackType]: values[feedbackType] + 1
-    }));
-  }
-
-  const resetFeedback = () => {
-    setValues({ good: 0, neutral: 0, bad: 0 })
-  }
-
+  
   return (
     <>
-      <Description />
-      <Options resetFeedback={resetFeedback} updateFeedback={updateFeedback} totalFeedback={totalFeedback}  />
-      {totalFeedback > 0
-        ? <Feedback values={values} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback} />
-        : <Notification />
-      }
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+      <Contact />
+      
     </>
   );
 };
