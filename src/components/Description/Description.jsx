a60fe4a770f42d2f1000daa99de28c5b1e61c350
Kostya Slobodianiuk <kostyaslob@gmail.com>
2025-05-01 16:45:46 +0300

    Initial commit

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,10 @@
+import css from "./Description.module.css"
+
+export default function Description() {
+    return (
+        <div className={css.description}>
+            <h2>Sip Happens Café</h2>
+            <p>Please leave your feedback about our service by selecting one of the options below.</p>
+      </div>
+    )
+}
